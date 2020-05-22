13b2514cd473a72b84c1e69b227e01812819438d
sj <10793793+satya64@users.noreply.github.com>
2020-05-22 19:25:25 +0530

    aws qs initial commit (#6)

---
+++ src/helpers/aws-quicksight.js
@@ -0,0 +1,17 @@
+const isEqual = require('lodash/isEqual');
+const config = require('../config');
+const AWS = require('aws-sdk');
+const Quicksight = require('aws-sdk/clients/quicksight');
+
+const awsConfig = config.aws;
+
+AWS.config.credentials = new AWS.CognitoIdentityCredentials(awsConfig.config.credentials);
+AWS.config.region = awsConfig.config.region;
+
+const quicksight = new Quicksight({
+    region: awsConfig.quicksight.region,
+});
+
+module.exports = {
+    quicksight,
+};
--- src/service/index.js
+++ src/service/index.js
@@ -1,9 +1,11 @@
 const authService = require('./auth-service');
 const formService = require('./form-service');
 const dashboardService = require('./dashboard-service');
+const quicksightService = require('./quicksight-report-service');
 
 module.exports = {
   authService,
   formService,
-  dashboardService
+  dashboardService,
+  quicksightService,
 };
